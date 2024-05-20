6489654fc1a6b69f63153ceeffab35b51e5d5710
Uğur Akın <dev.ugur.akin@gmail.com>
2024-05-20 20:06:00 +0300

    refactoring: for every valid URL, Build now calls getDocFromURL, then getLinksFromDoc

--- client.go
+++ client.go
@@ -1,7 +1,9 @@
 package sitemapbuilder
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"strings"
@@ -23,28 +25,28 @@ func Build(rawURL string) ([]lp.Link, error) {
 	// and is not a relative path
 	url, err := url.Parse(rawURL)
 	if err != nil {
-		return nil, fmt.Errorf("error parsing raw URL %s: %v", rawURL, err)
+		return nil, fmt.Errorf("error parsing raw URL (%s): %v", rawURL, err)
 	}
 
 	if !url.IsAbs() || url.Hostname() == "" {
 		return nil, fmt.Errorf("invalid root URL (%s): root URL must be absolute with a host, and not a relative path", rawURL)
 	}
 
-	links, err := getLinksFromURL(url)
+	doc, err := getDocFromURL(rawURL)
+	if err != nil {
+		return nil, err
+	}
+	links, err := getLinksFromDoc(url, doc)
 	if err != nil {
 		return nil, err
 	}
 	return links, nil
 }
 
-func getLinksFromURL(url *url.URL) ([]lp.Link, error) {
-	scheme := url.Scheme
-	host := url.Hostname()
-	// fmt.Printf("scheme: %s, host: %s\n", scheme, host)
-
+func getDocFromURL(url string) ([]byte, error) {
 	c := getClient()
 
-	resp, err := c.Get(url.String())
+	resp, err := c.Get(url)
 	if err != nil {
 		return nil, err
 	}
@@ -56,21 +58,32 @@ func getLinksFromURL(url *url.URL) ([]lp.Link, error) {
 	// fmt.Println(cType)
 
 	if resp.StatusCode == 200 && strings.Contains(cType, "text/html") {
-		allLinks, err := lp.Parse(resp.Body)
+		doc, err := io.ReadAll(resp.Body)
 		if err != nil {
-			return nil, fmt.Errorf("error parsing response body: %v", err)
+			return nil, fmt.Errorf("error reading response body: %v", err)
 		}
-		// only include links to the same host
-		var links []lp.Link
-		for _, l := range allLinks {
-			linkURL, _ := url.Parse(l.Href)
-			if linkURL.Scheme == scheme && linkURL.Hostname() == host && !strings.Contains(l.Href, "#") {
-				links = append(links, l)
-			}
-		}
-
-		return links, nil
+		return doc, nil
 	}
 
 	return nil, nil
+}
+
+func getLinksFromDoc(url *url.URL, doc []byte) ([]lp.Link, error) {
+	scheme := url.Scheme
+	host := url.Hostname()
+
+	allLinks, err := lp.Parse(bytes.NewReader(doc))
+	if err != nil {
+		return nil, fmt.Errorf("error getting links from doc: %v", err)
+	}
+	// only include links to the same host
+	var links []lp.Link
+	for _, l := range allLinks {
+		linkURL, _ := url.Parse(l.Href)
+		if linkURL.Scheme == scheme && linkURL.Hostname() == host && !strings.Contains(l.Href, "#") {
+			links = append(links, l)
+		}
+	}
+
+	return links, nil
 }
