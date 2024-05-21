8156f9bb3a3825e325d7588bc5948e9b61b4e78b
Uğur Akın <dev.ugur.akin@gmail.com>
2024-05-21 20:24:38 +0300

    depth flag only accepts an int >1 now

--- main.go
+++ main.go
@@ -10,8 +10,13 @@ import (
 
 func main() {
 	urlFlag := flag.String("u", "http://127.0.0.1:8080/", "the URL to build the sitemap for")
-	depth := flag.Int("d", 3, "the maximum number of links deep to traverse")
+	depth := flag.Int("d", 3, "the maximum depth of links to traverse. setting it to 1 means extracting from only the root URL u. Panics if d < 1.")
 	flag.Parse()
+
+	// panic if maxDepth < 1
+	if *depth < 1 {
+		panic("maxDepth must be 1 or higher.")
+	}
 
 	smb, err := sitemapbuilder.New(*urlFlag, *depth)
 	if err != nil {
